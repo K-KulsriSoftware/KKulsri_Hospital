1f9648a6807ea8bbb75c2df0fb6da78a7efb1193
Fruit <bee.smartboy@hotmail.com>
2017-09-28 22:13:14 +0700

    second checking

--- KKulsri_Hospital/app/static/app/scripts/doctor-search.js
+++ KKulsri_Hospital/app/static/app/scripts/doctor-search.js
@@ -1,19 +1,13 @@
 $('li#auto').click(function(){
-  if($('li#manual').hasClass('active')){
-    $('li#manual').removeClass('active');
-    $('div#manual-page').hide();
-    $('div#auto-page').removeClass('hide');
-  }
-  $('li#auto').addClass('active');
-
+  $(this).addClass('active');
+  $('li#manual').removeClass('active');
+  $('div#manual-page').addClass('hide')
+  $('div#auto-page').removeClass('hide')
 })
 
 $('li#manual').click(function(){
-  if($('li#auto').hasClass('active')){
-    $('li#auto').removeClass('active');
-    $('div#auto-page').hide();
-    $('div#manual-page').removeClass('hide');
-  }
-  $('li#auto').addClass('active');
-
+  $(this).addClass('active');
+  $('li#auto').removeClass('active');
+  $('div#auto-page').addClass('hide')
+  $('div#manual-page').removeClass('hide')
 })
