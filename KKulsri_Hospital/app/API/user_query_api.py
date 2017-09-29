d1b793b49b85d0e24fbb1328ccb3a3ee792a0579
Watcharachat PLANGSRI <watcharachat.p@live.ku.th>
2017-09-29 19:41:50 +0700

    added user_query_api.py

---
+++ KKulsri_Hospital/app/API/user_query_api.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pprint import pprint
+class user_query_api :
+
+	def __init__(self, db) :##
+		self.db = db
+
+	def get_all_users(self) :##
+		cursor = self.db.users.aggregate([
+			{
+            	'$match' : {}
+        	}
+		])
+		users = []
+		for user in cursor :
+			user.pop('_id', None)
+			users.append(user)
+		return True, users
+
+	def get_user_detail(self,username) :##
+		cursor = self.db.users.aggregate([
+			{
+            	'$match' : 
+            		{
+            			'username' : username
+            		}
+        	}
+		])
+		for user in cursor :
+			user.pop('_id', None)
+			return True, user
+		return False, "No match profile"
+
+	def get_all_users_name(self) :
+		cursor = self.db.users.aggregate([
+			{
+            	'$match' : {}
+        	},
+        	{
+        		'$project' : {
+        			'username' : '$username',
+        			'password' : '$password',
+        		}
+        	}
+		])
+		users = []
+		for user in cursor :
+			user.pop('_id', None)
+			users.append(user)
+		return True, users
+
+	def update_user_profile(self, username, password) :
+		if username == None or password == None :
+			return False, 'Incomplete input'
+		self.db.users.update_one(
+			{
+        		'username': username
+    		},
+    		{
+        		'$set': 
+        		{
+        			'password' : password,
+        		}
+    		}
+		)
+		return True, 'Successfully Updated'
+
+	def delete_user(self, username) :
+		if username == None :
+			return False, 'Incomplete input: username'
+		self.db.user.delete_one(
+			{
+				"username": username
+			}
+		)
+		return True, 'Successfully Removed'
