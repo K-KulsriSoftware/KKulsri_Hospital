f66feb9e5ab9539d27be77f3e596863ab82573bd
jirateep <charoenshop@gmail.com>
2017-09-30 19:17:37 +0700

    Delete common_function

--- KKulsri_Hospital/app/API/get_collection_pattern_api.py
+++ KKulsri_Hospital/app/API/get_collection_pattern_api.py
@@ -68,18 +68,366 @@ class get_collection_pattern_api :
 		return [
 			{
 				'field_name' : 'building_id',
-				'field_type' : 'integer'
+				'field_type' : 'int'
 			},
 			{
 				'field_name' : 'building_name',
-				'field_type' : 'integer'
+				'field_type' : 'int'
 			}
 		]
 	
+	def get_departments_pattern(self) :
+		return [
+			{
+				'field_name' : 'department_id',
+				'field_type' : 'int'
+			},
+			{
+				'field_name' : 'department_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'department_description',
+				'field_type' : 'string'
+			}
+		]
+
+	def get_departments_pattern(self) :
+		return [
+			{
+				'field_name' : 'username',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'doctor_name_title',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'doctor_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'doctor_surname',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'gender',
+				'field_type' : 'bool'
+			},
+			{
+				'field_name' : 'birthday',
+				'field_type' : 'date'
+			},
+			{
+				'field_name' : 'office_phone_number',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'email',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'department_id',
+				'field_type' : 'int'
+			},
+			{
+				'field_name' : 'doctor_img',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'position',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'expertises',
+				'field_type' : 'list',
+				'value' : 'string'
+			},
+			{
+				'field_name' : 'educations',
+				'field_type' : 'list',
+				'value' : 'string'
+			},
+			{
+				'field_name' : 'language',
+				'field_type' : 'list',
+				'value' : 'string'
+			},
+			{
+				'field_name' : 'working_time',
+				'field_type' : 'dict',
+				'dict' : 
+				[
+					{
+						'field_name' : 'mon',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'tue',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'wed',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'thu',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'fri',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'sat',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+					{
+						'field_name' : 'sun',
+						'field_type' : 'list',
+						'value' : 'dict'
+						'dict' :
+						[
+							{
+								'field_name' : 'start',
+								'field_type' : 'string'
+							},
+							{
+								'field_name' : 'finish',
+								'field_type' : 'string'
+							}
+						]
+					},
+				]
+			},
+			{
+				'field_name' : 'order_ids',
+				'field_type' : 'list',
+				'value' : 'string'
+			}
+		]
+
+	def get_patients_pattern(self) :
+		return [
+			{
+				'field_name' : 'username',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'patient_name_title',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'patient_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'patient_surname',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'patient_img',
+				'field_type' : 'bool'
+			},
+			{
+				'field_name' : 'id_card_number',
+				'field_type' : 'date'
+			},
+			{
+				'field_name' : 'gender',
+				'field_type' : 'bool'
+			},
+			{
+				'field_name' : 'order_ids',
+				'field_type' : 'list',
+				'value' : 'string'
+			},
+			{
+				'field_name' : 'birthday',
+				'field_type' : 'date'
+			},
+			{
+				'field_name' : 'blood_group_abo',
+				'field_type' : 'int'
+			},
+			{
+				'field_name' : 'blood_group_rh',
+				'field_type' : 'int',
+			},
+			{
+				'field_name' : 'race',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'nationallity',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'Religion',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'Status',
+				'field_type' : 'int'
+			},
+			{
+				'field_name' : 'patient_address',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'occupy',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'telphone_number',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'father_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'mother_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'emergency_name',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'emergency_address',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'email',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'congenital_disease',
+				'field_type' : 'list',
+				'value' : 'string'
+			}
+		]
+
+	def get_orders_pattern(self) :
+		return [
+			{
+				'field_name' : 'order_id',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'doctor_id',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'patient_id',
+				'field_type' : 'string'
+			},
+			{
+				'field_name' : 'building_id',
+				'field_type' : 'int'
+			},
+			{
+				'field_name' : 'cost',
+				'field_type' : 'double'
+			},
+			###############################
+			{
+				'field_name' : 'building_id',
+				'field_type' : 'int'
+			},
+		]
+
+	def get_packages_pattern(self) :
+		return [
+		]
+
 	def get_collection_pattern(self, collection_name) :
 		if collection_name == None :
 			return False, 'Incomplete input: collection_name'
-		elif collection_name == 'building' :
+		elif collection_name == 'buildings' :
 			return True, self.get_buildings_pattern()
+		elif collection_name == 'departments' :
+			return True, self.get_departments_pattern()
+		elif collection_name == 'doctors' :
+			return True, self.get_doctors_pattern()
+		elif collection_name == 'patients' :
+			return True, self.get_patients_pattern()
+		elif collection_name == 'orders' :
+			return True, self.get_orders_pattern()
+		elif collection_name == 'packages' :
+			return True, self.get_packages_pattern()
 		else :
 			return False, 'No collection name : ' + collection_name
