2afaa74da3758168d16736ce6f69e16932d5175c
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-10-01 11:23:45 +0700

    prepare data to search doctor

--- KKulsri_Hospital/app/static/app/scripts/doctor-search.js
+++ KKulsri_Hospital/app/static/app/scripts/doctor-search.js
@@ -10,10 +10,46 @@ $('li#manual').click(function(){
   $('li#auto').removeClass('active');
   $('div#auto-page').addClass('hide')
   $('div#manual-page').removeClass('hide')
-})
+});
 
 $('.btn-summit').click(function() {
   $('#result').empty();
+  var data = {};
+  data.days = '';
+  $('.input-days input[type="checkbox"]').each(function() {
+    if($(this).prop('checked')) {
+      data.days += $(this).attr('id').substring(0, 3) + ','
+    }
+  });
+  if(data.days === '') {
+    delete data.days;
+  }
+  data.time = '';
+  $('.input-time input[type="checkbox"]').each(function() {
+    if($(this).prop('checked')) {
+      data.time += $(this).closest('label').text().replace(/ /g, '').replace(/\n/g, '') + ',';
+    }
+  });
+  if(data.time === '') {
+    delete data.time;
+  }
+  data.doctor_firstname = $('.input-name input#in-firstname').val();
+  if(data.doctor_firstname === '') {
+    delete data.doctor_firstname;
+  }
+  data.doctor_surname = $('.input-name input#in-surname').val();
+  if(data.doctor_surname === '') {
+    delete data.doctor_surname;
+  }
+  data.gender = '';
+  $('.input-gender input[type="checkbox"]').each(function() {
+    if($(this).prop('checked')) {
+      data.gender += $(this).closest('label').text().replace(/ /g, '').replace(/\n/g, '') + ',';
+    }
+  });
+  if(data.gender === '') {
+    delete data.gender;
+  }
   $.get('/doctor_search_api', {}, function(data) {
     if(data && data.status && data.result.length > 0) {
       $(data.result).each(function() {
@@ -24,7 +60,7 @@ $('.btn-summit').click(function() {
           <button class="btn btn-default btn-res" type="button">นัดหมายแพทย์</button>
           </div>`
         );
-      })
+      });
     }
-  })
+  });
 })
