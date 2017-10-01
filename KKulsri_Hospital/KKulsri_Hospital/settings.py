b1decaabdb9a1c7a156273320bde07216072de16
Siwanont Sittinam <siwanont1@gmail.com>
2017-10-01 23:40:06 +0700

    update path to uwsgi

--- KKulsri_Hospital/KKulsri_Hospital/settings.py
+++ KKulsri_Hospital/KKulsri_Hospital/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = 'home'
 
